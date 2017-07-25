3bf6094c4032fc5226c12d9f877b76b5c31fa529
vamsi krishna <vamc.koduri@gmail.com>
2017-07-25 10:23:43 +0530

    modified index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React from 'react';
+      import React from 'react';
+
+      class App  extends React.Component{
+
+        constructor(){
+          super();
+          this.state = {
+            txt : 'This is the state txt',
+            cat : 99
+          }
+        }
+        update (e)
+        {
+          this.setState({txt:e.target.value})
+
+        }
+
+        render (){
+          return( 
+
+            <div> 
+            <h1> hello world</h1>   
+            <input type="text" onChange={this.update.bind(this)} />
+            <h1> {this.state.txt} - {this.state.cat} </h1>
+            </div>
+
+            
+            )
+        }
+      }
 
 
-//const App = () => <h1> Hello vamsi </h1>
-
-class App  extends React.Component
-{
-  render (){
-
-   // return React.createElement('h1',null,'hello world 2600 ')
-   let txt = this.props.txt
-      return( <h1>{txt} {this.props.cat} {this.props.howdy} {this.props.temp}</h1>   )
-  }
-}
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-  
-
-}
-
-App.defaultProps = {
-  txt: " this is the new default text ",
-  howdy: " how do you do ",
-  temp: "this is a 3465 text"
-}
-
-export default App;
+      export default App;
