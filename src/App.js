3a99c5485c5cc25a50dc65dce027e4417b0cd995
vamsi krishna <vamc.koduri@gmail.com>
2017-07-25 08:21:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+//const App = () => <h1> Hello vamsi </h1>
+
+class App  extends React.Component
+{
+  render (){
+
+   // return React.createElement('h1',null,'hello world 2600 ')
+      return <h1>Hello Vamsi </h1>   
+
+  }
+}
+
+export default App;
