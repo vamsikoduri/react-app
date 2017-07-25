da3d016db5986b3c6b0009e8b4f2c996084d97c6
vamsi krishna <vamc.koduri@gmail.com>
2017-07-25 10:03:58 +0530

    passing parameters

--- src/App.js
+++ src/App.js
@@ -8,9 +8,22 @@ class App  extends React.Component
   render (){
 
    // return React.createElement('h1',null,'hello world 2600 ')
-      return <h1>Hello Vamsi </h1>   
-
+   let txt = this.props.txt
+      return( <h1>{txt} {this.props.cat} {this.props.howdy} {this.props.temp}</h1>   )
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+  
+
+}
+
+App.defaultProps = {
+  txt: " this is the new default text ",
+  howdy: " how do you do ",
+  temp: "this is a 3465 text"
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+	<App  txt={"this is a latest  prop text " } cat={5} />,
+	document.getElementById('root')
+
+	);
 
